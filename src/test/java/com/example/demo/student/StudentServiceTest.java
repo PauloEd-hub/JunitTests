c31ae34b0838b3e8f10320dfe07dc19b8c0d56b1
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-02-21 21:29:33 -0300

    Criação de teste para o método que adiciona estudantes.

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,14 +1,21 @@
 package com.example.demo.student;
 
-import org.junit.jupiter.api.AfterEach;
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -36,8 +43,49 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
     void addStudent() {
+
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                Gender.FEMALE
+        );
+
+        //when
+        underTest.addStudent(student);
+
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        Mockito.verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
+    }
+
+
+    @Test
+    void willThrowWhenEmailIsTaken() {
+
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                Gender.FEMALE
+        );
+
+        given(studentRepository.selectExistsEmail(anyString()))
+                .willReturn(true);
+
+        //when
+        //then
+        assertThatThrownBy(() -> underTest.addStudent(student))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + student.getEmail() + " taken");
+
+        Mockito.verify(studentRepository, never()).save(any());
+
+
     }
 
     @Test
